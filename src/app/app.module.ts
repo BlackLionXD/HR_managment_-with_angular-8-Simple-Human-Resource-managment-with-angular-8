92e271e0af25d51d74d59ca95654b5e2ec2fbf1f
BlackLionXD <ibrahimgidi2019@gmail.com>
2023-05-08 12:01:24 +0300

    Component Created Success

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { DeleteConfirmationComponent } from './components/delete-confirmation/delete-confirmation.component';
+import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component';
+import { ViewEmployeeComponent } from './components/view-employee/view-employee.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    PageNotFoundComponent,
+    DeleteConfirmationComponent,
+    EditEmployeeComponent,
+    ViewEmployeeComponent,
+    NavbarComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule
